c94347b25b80dfee83310c001a1a94ef3ac518b1
Stephen Donald Armstrong <58577114+Stephen-Armstrong@users.noreply.github.com>
2025-10-23 18:20:07 -0500

    Doing a run of all of the options to see if the same issue is happening in all of them or just the no OpenMP, no CUDA version.

--- campuscluster_update.py
+++ campuscluster_update.py
@@ -34,8 +34,8 @@ cuda_module = "cuda/12.8" #11.6
 python_module = "python/3.13.2"
 cmake_version = "3.26.5"
 
-openmp_options = [False] #[True, False]
-cuda_arch_options = [None] #[None, 70, 86, 90]
+openmp_options = [True, False]
+cuda_arch_options = [None, 70, 86, 90]
 build_types = ["Debug", "Release"] #["Debug", "Release"]
 
 
